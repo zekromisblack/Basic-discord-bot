1ebc788a4b811e500c5fe843dc2b6a8dc57396b2
zekromisblack <55082829+zekromisblack@users.noreply.github.com>
2020-03-25 23:55:56 +1030

    very basic bot
    pretty cool if you're looking into coding your own discord.py bot

---
+++ bot2.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+bot=commands.Bot(command_prefix="!")
+@bot.command(name="ping")
+async def ping_command_handler(ctx):
+    await ctx.send("pong!")
+asyncio.get_event_loop().run_until_complete(bot.start("tokenhere", bot=True))
